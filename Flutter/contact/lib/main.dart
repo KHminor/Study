0885067a6cbb1de6341ffa1e06849ac6e4ae2048
KHminor <ghdals2020@gmail.com>
2023-07-29 22:26:30 +0900

    Flutter

--- Flutter/contact/lib/main.dart
+++ Flutter/contact/lib/main.dart
@@ -1,67 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  //runApp 안에 있는 MyApp을 실행해달라는 코드
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  var a = 1;
+  var name = ['메시', '날강두', '홀란'];
+  var cnt = [0, 0, 0];
 
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          // title은 왼쪽 제목, leading은 왼쪽에 넣을 아이콘을 의미
-          // actions는 우측에 넣을 아이콘들
-          backgroundColor: Colors.white,
-          title: Text('금호동3가', style: TextStyle(color: Colors.black, fontWeight: FontWeight.w900)),
-          actions: [
-            Row(
-              children: [Icon(Icons.search), SizedBox(width: 20), Icon(Icons.menu), SizedBox(width: 20), Icon(Icons.notifications_none_sharp)],
-            )
-          ],
-          actionsIconTheme: IconThemeData(color: Colors.black, size: 40),
-          leading: Icon(Icons.access_time),
-        ),
-        body: Container(
-          child: Row(
-            children: [
-              Expanded(
-                flex: 3,
-                child: Container(
-                  height: 300,
-                  alignment: Alignment.topCenter,
-                  child: Image.asset('JJangu.jpg'),
-                  margin: EdgeInsets.fromLTRB(15, 30, 15, 0),
+        appBar: AppBar(),
+        body: ListView.builder(
+            itemCount: 3,
+            itemBuilder: (c,i){
+              return ListTile(
+                leading: Text(cnt[i].toString()),
+                title: Text(name[i]),
+                trailing: TextButton(
+                  child: Text('좋아요'),
+                  onPressed: (){
+                    setState(() {
+                      cnt[i]++;
+                    });
+                  },
                 ),
-              ),
-              Expanded(
-                flex: 7,
-                child: Container(
-                  height: 300,
-                  margin: EdgeInsets.fromLTRB(15, 30, 15, 0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)', style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700)),
-                      Text('성동구 행당동 끌올 10분 전', style: TextStyle(fontSize: 12, color: Colors.grey, fontWeight: FontWeight.w700)),
-                      Text('210.000원', style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Icon(Icons.favorite_border, color: Colors.grey),
-                          Text('4', style: TextStyle(fontSize: 20, color: Colors.grey, fontWeight: FontWeight.w300),)
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+              );
+            }
         ),
       ),
     );
