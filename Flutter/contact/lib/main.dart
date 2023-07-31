26743f6f044620b31dd560b380392c2711acc686
KHminor <ghdals2020@gmail.com>
2023-07-31 22:28:33 +0900

    Flutter

--- Flutter/contact/lib/main.dart
+++ Flutter/contact/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      MaterialApp(
+        home: MyApp()
+      )
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -13,32 +17,122 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
 
   var a = 1;
-  var name = ['메시', '날강두', '홀란'];
-  var cnt = [0, 0, 0];
+  var total = 3;
+  var person = [['메시', 0, '010-1234-5678'], ['날강두', 0, '010-1234-5678'], ['홀란', 0, '010-1234-5678']];
+  // var cnt = [0, 0, 0];
+  var title = 'Contact';
+
+  changeNumber(){
+    setState(() {
+      total++;
+    });
+  }
+
+  changeState(value){
+    setState(() {
+      person.add(value);
+    });
+  }
+
+  deleteState(idx){
+    setState(() {
+      person.removeAt(idx);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          showDialog(context: context, builder: (context){
+            return Dialog(
+              child: Modal(state: title, changeNumber: changeNumber, changeState:changeState)
+            );
+          });
+        },
+      ),
+      appBar: AppBar(title: Text(total.toString()),),
+      body: ListView.builder(
+          itemCount: person.length,
+          itemBuilder: (c,i){
+            return ListTile(
+              leading: Text(person[i][1].toString()),
+              title: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Text(person[i][0].toString()),
+                  Text('     '),
+                  Text(person[i][2].toString()),
+                  Text(' '),
+                  TextButton(onPressed: (){
+                    deleteState(i);
+                  }, child: Text('삭제'))
+                ],
+              ),
+              trailing: TextButton(
+                child: Text('좋아요'),
+                onPressed: (){
+                  setState(() {
+                    person[i][1] = (person[i][1] as int) +1;
+                  });
+                },
+              ),
+            );
+          }
+      ),
+    );
+  }
+}
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: ListView.builder(
-            itemCount: 3,
-            itemBuilder: (c,i){
-              return ListTile(
-                leading: Text(cnt[i].toString()),
-                title: Text(name[i]),
-                trailing: TextButton(
-                  child: Text('좋아요'),
-                  onPressed: (){
-                    setState(() {
-                      cnt[i]++;
-                    });
-                  },
+class Modal extends StatelessWidget {
+  Modal({super.key, this.state, this.changeNumber, this.changeState});
+  final state;
+  final changeNumber;
+  var changeState;
+  var inputData = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 400,
+      margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.all(30),
+            child: Text(state, style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900)),
+          ),
+          Container(
+            margin: EdgeInsets.all(30),
+            child: TextField( controller: inputData, style: TextStyle(fontSize: 30),),
+          ),
+          Container(
+            margin: EdgeInsets.all(30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: TextButton(onPressed: (){
+                    Navigator.pop(context);
+                  }, child: Text('Cencel', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)),
                 ),
-              );
-            }
-        ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: TextButton(onPressed: (){
+                    if (inputData.text != ''){
+                      changeState([inputData.text.toString(), 0, '010-1234-5678']);
+                      Navigator.pop(context);
+                    }
+                  }, child: Text('OK', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
