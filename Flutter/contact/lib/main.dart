fd74a1c1bdee78715e22b75aed494ddc054fd20a
KHminor <ghdals2020@gmail.com>
2023-08-08 19:01:46 +0900

    flutter

--- Flutter/contact/lib/main.dart
+++ Flutter/contact/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:contacts_service/contacts_service.dart';
 
 void main() {
   runApp(
@@ -16,9 +18,31 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
+  getPermission() async{
+    var status = await Permission.contacts.status; // 연락처 권한을 주었는지 대한 여부
+    if (status.isGranted) {
+      print('허락됨');
+      var contacts = await ContactsService.getContacts();
+      setState(() {
+        person = contacts;
+      });
+    } else if (status.isDenied) {
+      print('거절됨');
+      Permission.contacts.request(); // 허락해달라고 팝업띄우는 코드
+    }
+  }
+
+  @override
+  void initState() { // 위젯이 로드될 때 한번 실행
+    super.initState();
+    getPermission();
+    // getPermission();
+    // openAppSettings();
+  }
+
   var a = 1;
   var total = 3;
-  var person = [['메시', 0, '010-1234-5678'], ['날강두', 0, '010-1234-5678'], ['홀란', 0, '010-1234-5678']];
+  var person = [];
   // var cnt = [0, 0, 0];
   var title = 'Contact';
 
@@ -47,37 +71,22 @@ class _MyAppState extends State<MyApp> {
         onPressed: (){
           showDialog(context: context, builder: (context){
             return Dialog(
-              child: Modal(state: title, changeNumber: changeNumber, changeState:changeState)
+              child: Modal(state: title, changeNumber: changeNumber, changeState:changeState, getPermission:getPermission)
             );
           });
         },
       ),
-      appBar: AppBar(title: Text(total.toString()),),
+      appBar: AppBar(title: Text(person.length.toString()),actions: [
+        IconButton(onPressed: (){
+          getPermission();
+        }, icon: Icon(Icons.contacts))
+      ],),
       body: ListView.builder(
           itemCount: person.length,
           itemBuilder: (c,i){
             return ListTile(
-              leading: Text(person[i][1].toString()),
-              title: Row(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(person[i][0].toString()),
-                  Text('     '),
-                  Text(person[i][2].toString()),
-                  Text(' '),
-                  TextButton(onPressed: (){
-                    deleteState(i);
-                  }, child: Text('삭제'))
-                ],
-              ),
-              trailing: TextButton(
-                child: Text('좋아요'),
-                onPressed: (){
-                  setState(() {
-                    person[i][1] = (person[i][1] as int) +1;
-                  });
-                },
-              ),
+              leading: Icon(Icons.person),
+              title: Text(person[i].givenName??'무명')
             );
           }
       ),
@@ -86,9 +95,10 @@ class _MyAppState extends State<MyApp> {
 }
 
 class Modal extends StatelessWidget {
-  Modal({super.key, this.state, this.changeNumber, this.changeState});
+  Modal({super.key, this.state, this.changeNumber, this.changeState, this.getPermission});
   final state;
   final changeNumber;
+  final getPermission;
   var changeState;
   var inputData = TextEditingController();
 
@@ -96,35 +106,48 @@ class Modal extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 400,
-      margin: EdgeInsets.fromLTRB(30, 0, 30, 0),
+      margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            margin: EdgeInsets.all(30),
-            child: Text(state, style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900)),
+            margin: EdgeInsets.all(5),
+            child: Text(state, style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900)),
           ),
           Container(
-            margin: EdgeInsets.all(30),
-            child: TextField( controller: inputData, style: TextStyle(fontSize: 30),),
+            margin: EdgeInsets.all(5),
+              controller: inputData,
+              style: TextStyle(fontSize: 30),
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(),
+                icon:Icon(Icons.ac_unit_sharp),
+                  hintText: "첫 째",
+                  helperText: "둘 째",
+                  labelText: '셋 째',
+                  counterText: '넷 째'
+              ),
+            ),
           ),
           Container(
-            margin: EdgeInsets.all(30),
+            margin: EdgeInsets.all(5),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Container(
-                  margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
                   child: TextButton(onPressed: (){
                     Navigator.pop(context);
                   }, child: Text('Cencel', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)),
                 ),
                 Container(
-                  margin: EdgeInsets.fromLTRB(20, 0, 20, 0),
-                  child: TextButton(onPressed: (){
+                  margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                  child: TextButton(onPressed: () async{
                     if (inputData.text != ''){
-                      changeState([inputData.text.toString(), 0, '010-1234-5678']);
+                      var newPerson = Contact();
+                      newPerson.givenName = inputData.text.toString();
+                      await ContactsService.addContact(newPerson);
+                      await getPermission();
                       Navigator.pop(context);
                     }
                   }, child: Text('OK', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)),
