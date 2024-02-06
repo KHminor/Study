e9291f5af183df7cfc26e19586109d296202f69b
KHminor <ghdals2020@gmail.com>
2024-02-06 18:19:40 +0900

    canvas

--- canvas/spark/js/Particle.js
+++ canvas/spark/js/Particle.js
@@ -1,26 +1,35 @@
 import CanvasOption from "./CanvasOption.js";
 
 export default class Particle extends CanvasOption {
-  constructor(x, y, vx, vy) {
+  constructor(x, y, vx, vy, opacity, colorDeg) {
     super();
     this.x = x;
     this.y = y;
     this.vx = vx;
     this.vy = vy;
-    this.opacity = 1;
+    this.opacity = opacity;
+    this.gravity = 0.12;
+    this.friction = 0.93;
+    this.colorDeg = colorDeg;
   }
 
   update() {
+    this.vy += this.gravity;
+
+    this.vx *= this.friction;
+    this.vy *= this.friction;
+
     this.x += this.vx;
     this.y += this.vy;
 
-    this.opacity -= 0.01;
+    this.opacity -= 0.014;
   }
 
   draw() {
-    this.ctx.fillStyle = `rgba(255,255,255,${this.opacity}`;
+    console.log(this.x, this.y, this.vx, this.vy, this.opacity, this.colorDeg);
+    this.ctx.fillStyle = `hsla(${this.colorDeg},100%,65%, ${this.opacity})`;
     this.ctx.beginPath();
-    this.ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
+    this.ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
     this.ctx.fill();
     this.ctx.closePath();
   }
